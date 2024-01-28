7d34bd1d0b31d16f6efb5f1dc193a0535dc68e6a
oGuiSM <157987220+oGuiSM@users.noreply.github.com>
2024-01-28 20:12:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var message = document.querySelector('.message');
     var countdown = document.getElementById('countdown');
-    var message = document.getElementById('message');
-    var success = document.getElementById('success');
+    var hearts = document.querySelectorAll('.heart');
+    var word1 = document.getElementById('word1');
+    var word2 = document.getElementById('word2');
+    var word3 = document.getElementById('word3');
+    var heart = document.getElementById('heart');
 
-    var timer = 10; // Tempo inicial da contagem regressiva
-
+    // Contagem regressiva de 10 segundos
+    var timer = 10;
     var countdownInterval = setInterval(function () {
         timer--;
-        countdown.textContent = timer;
         if (timer <= 0) {
             clearInterval(countdownInterval);
-            countdown.style.display = 'none';
-            message.style.display = 'block';
+            message.style.display = 'none';
+            removeHearts();
+            showWords();
+        } else {
+            hearts[timer].style.opacity = '0';
         }
     }, 1000);
+
+    // Remover os corações após a contagem regressiva
+    function removeHearts() {
+        document.getElementById('hearts').style.display = 'none';
+    }
+
+    // Exibir as palavras uma após a outra
+    function showWords() {
+        setTimeout(function() {
+            word1.classList.remove('hidden');
+        }, 0);
+        setTimeout(function() {
+            word1.classList.add('hidden');
+            word2.classList.remove('hidden');
+        }, 2000);
+        setTimeout(function() {
+            word2.classList.add('hidden');
+            word3.classList.remove('hidden');
+        }, 4000);
+        setTimeout(function() {
+            word3.classList.add('hidden');
+            heart.classList.remove('hidden');
+        }, 6000);
+    }
 });
-
-function conquistar() {
-    var message = document.getElementById('message');
-    var success = document.getElementById('success');
-    var body = document.body;
-
-    message.style.display = 'none';
-    success.style.display = 'block';
-    body.style.backgroundImage = "url('https://img.freepik.com/fotos-premium/mao-fazendo-um-conceito-de-forma-de-coracao-amor-e-relacionamento-gesto_33745-540.jpg')";
-}
